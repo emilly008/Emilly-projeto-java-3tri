a52b0e727837735ffa1d5b004b38f4a77424f423
emilly008 <110101370+emilly008@users.noreply.github.com>
2023-10-11 11:29:05 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-document.querySelector('.tecla_pom');
+function tocaSom (idElementAudio) {
+    document.querySelector(idElementAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+    
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom('#som_tecla_pom')
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
